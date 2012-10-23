bb2b715bc17aa85c70652f6be5f95a4270eb4102
Rafael Abreu <rafaelf.p.abreu@gmail.com>
2012-10-23 11:52:42 +0100

    Inicio do Blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd27d36659ac3b60870d59d73f58683453bf3343e85f5455655eba07b6f47c546e7d7dbf023356a6dc0ee0fdd8c70bf4ad7cfd1e5a4772fcaa2b48c61cb8aa7f6'
